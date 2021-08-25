2025e34c1e5f8ed38c38906ee0c830a645deb2c1
Kihil <mirz1723@gmail.com>
2021-08-25 09:04:36 +0700

    Start learning C dicoding

---
+++ learnC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int
+
+main(void)
+{
+    printf("Hello Guys\n");
+    int i = 5; // pengisian dengan inisialisasi
+    printf("nilai i adalah: %d \n", i);
+
+    int a;
+    a = 5;
+    printf("nilai a adalah: %d \n", a);
+}
